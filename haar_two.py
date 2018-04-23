361fc72035bfdb580e3eba86461a9b03ec983e7b
McKennon McMillian <mbmcmill@go.olemiss.edu>
2018-04-23 12:00:08 -0500

    Calculate difference

--- haar_two.py
+++ haar_two.py
@@ -1,5 +1,7 @@
 import random
 cof_removed = 0
+cof_number = 0
+diff = 0
 def reduce_haar(list_to_reduce, diff_list):
 	first = []
 	second = []
@@ -39,21 +41,28 @@ def restore_haar(list_to_restore, diff_list):
 
 def remove_cof(diff_list):
 	global cof_removed
+	global cof_number
+	cof_number = len(diff_list)
 	thresh = 10
 	for i in range(round(len(diff_list)/2),len(diff_list)):
 		if diff_list[i] <= thresh and diff_list[i] >= -thresh:
-			diff_list[i] = 0
+			diff_list[i] = 0.0
 			cof_removed += 1
 	return diff_list
 
-test_list = [random.randint(0, 100) for x in range(262144)]#2^18
-print(test_list)
+test_list = [float(random.randint(0, 100)) for x in range(262144)]#2^18
+# print(test_list)
 test = reduce_haar(test_list, [])
 test = list(test)
 test[1] = remove_cof(test[1]) 
 print('Result of reduction:', test)
 # if test[0][0] == sum(test_list)/len(test_list):
 val = restore_haar(test[0],test[1])
-print('Result of restoration:', cof_removed, val[0][:10], test_list[:10])
+for i in range(len(test_list)):
+	if test_list[i] != val[0][i]:
+		diff += 1
+diff_ave = (diff/len(test_list)) * 100
+cof_ave = ((cof_removed/cof_number)) * 100
+print('Result of restoration:', cof_removed, val[0][:10], test_list[:10],'\n%'+str(diff_ave)+' differnt\n% of cof diff =', cof_ave)
 # else:
 # 	print('Reduced list and average not the same.')
