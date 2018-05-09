0e95b9716e4f20f729636fcb9c9abdb57e4c5f5d
McKennon McMillian <mbmcmill@go.olemiss.edu>
2018-05-08 22:12:21 -0500

    Add error.py

---
+++ error.py
@@ -0,0 +1,21 @@
+import math
+check = ['haar0', 'haar1', 'haar5', 'haar10', 'haar15', 'haar20', 'haar_sine_1', 'haar_sine_6', 'haar_sine_9', 'haar_test']
+e_list = []
+for x in check:
+	name = x
+	file = open('./data/'+x+'.txt', 'r')
+	l = file.readlines()[:-2]
+	l = l[1:]
+	l = [i.strip('\n\t') for i in l]
+	l = [i.split('\t') for i in l]
+	x = list(range(0, len(l)))
+	original = []
+	reconstructed = []
+	diff = []
+	for i in l:
+		original.append(float(i[0]))
+		reconstructed.append(float(i[1]))
+		if len(i) == 3:
+			diff.append(float(i[2]) ** 2)
+	e_list.append((name,len(diff),(len(diff)/len(l))*100,math.sqrt(sum(diff)/len(l))))
+print(*e_list,sep='\n')
